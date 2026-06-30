2e64d00a3f922b0680da4bb32538729a49202b08
pallavi shinde <pallavi shinde>
2026-06-30 14:50:25 -0700

    first python code

---
+++ marks.py
@@ -0,0 +1,12 @@
+student_name = "Karan"
+marks = 85     
+is_passed = True
+     
+print(student_name)
+print(marks)
+print(is_passed)
+     
+if marks >= 75:
+         print(f"{student_name} has passed exam")
+else:
+         print(f"{student_name} has failed the exam")
