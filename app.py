7d79f24e111207cc66f9fb81e43d8a087ba1ba0b
pallavi <ps3525142@gmail.com>
2026-07-14 18:30:53 -0700

    my project.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Welcome to My project</h1>'
+    return '<h1>Student Registration & Result Management System</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
